62c1b4a67fd2ead218863086ccca565709fd65ad
Ilya Cherevko <ya.titanikum@yandex.ru>
2022-02-06 12:21:32 +0400

    Проект mesto 09

--- scripts/index.js
+++ scripts/index.js
@@ -27,23 +27,23 @@ const initialCards = [
 
 
 //Переменные
-let popupEditProfile = document.querySelector('.popup_edit-form');                     // попап редактирования профиля
-let openPopupButton = document.querySelector('.profile__edit-button');                 // попап кнопка редактировать профиль
-let popupCloseButton = document.querySelector('.popup__close-button');                 // закрытие окна редактирования профиля
-let popupName = document.querySelector('.popup__input_profile_name');                  // редактирование профиля - имя
-let popupJob = document.querySelector('.popup__input_profile_job');                    // редактирование профиля - профессия
-let profileName = document.querySelector('.profile__name');                            // имя в шапке
-let profileJob = document.querySelector('.profile__job');                              // профессия в шапке
-let cardsList = document.querySelector('.elements');                                   // html ставим сюда
-let poupEditPlace = document.querySelector('.popup_add-form');                         // попап новое место
-let openProfileButton = document.querySelector('.profile__add-button');                // добавить данные в профиль
-let popupCloseForm = document.querySelector('.popup__close-button_add-form');          // закрытие окна нового места
-let poupPlaceName = document.querySelector('.popup__input_plase_name');                // добавление названия новой картинки
-let poupPlaceLink = document.querySelector('.popup__input_plase_link');                // добавление новой картинки
-let popupBigImage = document.querySelector('.popup_image-form');                   // попап большой картинки
-let popupCloseBigImage = document.querySelector('.popup__close-button_image_preview'); // закрытие большой картинки
-let nameBigImage = document.querySelector('.popup__image-big');                        // большая картинка
-let captionBigImage = document.querySelector('.popup__image-caption');                 // подпись большой картинки
+const profileEditPopup = document.querySelector('.popup_edit-form');                            // попап редактирования профиля
+const profileButtonOpenPopup = document.querySelector('.profile__edit-button');                 // попап кнопка редактировать профиль
+const profileButtonClosePopup = document.querySelector('.popup__close-button');                 // закрытие окна редактирования профиля
+const popupName = document.querySelector('.popup__input_profile_name');                         // редактирование профиля - имя
+const popupJob = document.querySelector('.popup__input_profile_job');                           // редактирование профиля - профессия
+const profileName = document.querySelector('.profile__name');                                   // имя в шапке
+const profileJob = document.querySelector('.profile__job');                                     // профессия в шапке
+const cardsList = document.querySelector('.elements');                                          // html ставим сюда
+const plaseEditPopup = document.querySelector('.popup_add-form');                               // попап новое место
+const profileButtonAddPopup = document.querySelector('.profile__add-button');                   // добавить данные в профиль
+const plaseButtonClosePopup = document.querySelector('.popup__close-button_add-form');          // закрытие окна нового места
+const poupPlaceName = document.querySelector('.popup__input_plase_name');                       // добавление названия новой картинки
+const poupPlaceLink = document.querySelector('.popup__input_plase_link');                       // добавление новой картинки
+const imagePreviewBigPopup = document.querySelector('.popup_image-form');                       // попап большой картинки
+const imagePreviewBigClosePopup = document.querySelector('.popup__close-button_image_preview'); // закрытие большой картинки
+const nameBigImage = document.querySelector('.popup__image-big');                               // большая картинка
+const captionBigImage = document.querySelector('.popup__image-caption');                        // подпись большой картинки
 
 // открытие попапа
 function openPopup(popup) {
@@ -56,72 +56,74 @@ function closePopup(popup) {
 }
 
 // Попап редактирования профиля. Сначала заносим данные в поля инпутов и потом открывыем попап профиля.
-openPopupButton.addEventListener('click', function () {
+profileButtonOpenPopup.addEventListener('click', function () {
   popupName.value = profileName.textContent;
   popupJob.value = profileJob.textContent;
-  openPopup(popupEditProfile);
+  openPopup(profileEditPopup);
 }); 
 
 // Закрытие попапа профиля
-popupCloseButton.addEventListener('click', function () {
-  closePopup(popupEditProfile);
+profileButtonClosePopup.addEventListener('click', function () {
+  closePopup(profileEditPopup);
 });
 
 // Попап добавления нового места. Сначала очищаем поля инпутов и потом открываем попап.
-openProfileButton.addEventListener('click', function () {
-  poupPlaceName.value = '';
-  poupPlaceLink.value = '';
-  openPopup(poupEditPlace);
+profileButtonAddPopup.addEventListener('click', function () {
+  // poupPlaceName.value = '';
+  // poupPlaceLink.value = '';
+  openPopup(plaseEditPopup);
 }); 
 
 // Закрытие попапа места
-popupCloseForm.addEventListener('click', function () {
-  closePopup(poupEditPlace);
+plaseButtonClosePopup.addEventListener('click', function () {
+  closePopup(plaseEditPopup);
 });
 
 // Попап превью картинки
-function cardPreview(event) {
+/*function previewCard (event) {
   // Название места и картинка лежат в разных контейнерах и соответственно соседних DOM узлах,
   // событие происходит в контейнере с картинкой и как достучаться до контейнера с названием я хз,
   // поэтому беру название из alt картинки, благо мы сами его туда и засунули
   captionBigImage.textContent = event.target.alt; 
   nameBigImage.alt = event.target.alt;
   nameBigImage.src = event.target.src;
-  openPopup(popupBigImage);
-}
+  openPopup(imagePreviewBigPopup);
+}*/
 
 //Закрытие попапа превью картинки
-popupCloseBigImage.addEventListener('click', function () {
-  closePopup(popupBigImage);
+imagePreviewBigClosePopup.addEventListener('click', function () {
+  closePopup(imagePreviewBigPopup);
 });
 
 // удаление карточки
-function CardRemove(event) {
+function removeCard (event) {
   event.target.closest('.element').remove();
 }
 
 // лайк на карточке
-function cardLike(event) {
+function likeCard (event) {
   event.target.classList.toggle('element__like_aktive');
 }
 
 // Сохранение и отправка изменений профиля
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
-function formSubmitHandler(evt) {
+function handleProfileFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileJob.textContent = popupJob.value;
-  closePopup(popupEditProfile);  
+  closePopup(profileEditPopup);  
 }
 
 // добавление карточек
-function cardSubmitList(evt) {
+function handleCardSubmit (evt) {
   evt.preventDefault();
   // передаeм функции значения инпутов попапа
   const newCard = creatNewCard(poupPlaceName.value, poupPlaceLink.value); 
   // добавление новой карточки
   addObject(cardsList, newCard);
-  closePopup(poupEditPlace);
+  poupPlaceName.value = '';
+  poupPlaceLink.value = '';
+  closePopup(plaseEditPopup);
 }
 
 // добавление html из темплейта в начало списка
@@ -133,21 +135,33 @@ function addObject(placeInHtml, object) {
 function creatNewCard(cardName, cardLink) {
   
   // content - берет только внутренности темплэйта
-  let cardTemplate = document.querySelector('.item__template').content; 
+  const cardTemplate = document.querySelector('.item__template').content; 
   
   // клонирование шаблона карточки в переменную
-  let newCard = cardTemplate.querySelector('.element').cloneNode(true); 
+  const newCard = cardTemplate.querySelector('.element').cloneNode(true); 
+  
+  // найдем картинку один раз, а исползуем три )
+  const newImage = newCard.querySelector('.element__image');
   
   // заполним название, alt и src
   newCard.querySelector('.element__name').textContent = cardName; 
-  newCard.querySelector('.element__image').alt = cardName; 
-  newCard.querySelector('.element__image').src = cardLink;
+  newImage.alt = cardName;
+  newImage.src = cardLink;
   
   //слушатели
-  newCard.querySelector('.element__trash').addEventListener('click', CardRemove);
-  newCard.querySelector('.element__like').addEventListener('click', cardLike);
-  newCard.querySelector('.element__image').addEventListener('click', cardPreview);
-    
+  newCard.querySelector('.element__trash').addEventListener('click', removeCard);
+  newCard.querySelector('.element__like').addEventListener('click', likeCard);
+  /*newImage.addEventListener('click', previewCard);*/
+  newImage.addEventListener('click', () => handleCardClick(cardName, cardLink));
+  
+  // Попап превью картинки теперь здесь
+  function handleCardClick () {
+    captionBigImage.textContent = cardName; 
+    nameBigImage.alt = cardName;
+    nameBigImage.src = cardLink;
+    openPopup(imagePreviewBigPopup);
+  }
+
   return newCard;
 }
 
@@ -158,5 +172,5 @@ initialCards.forEach((card) => {
 });
 
 // Обработчики событий
-popupEditProfile.addEventListener('submit', formSubmitHandler);
-poupEditPlace.addEventListener('submit', cardSubmitList);
+profileEditPopup.addEventListener('submit', handleProfileFormSubmit);
+plaseEditPopup.addEventListener('submit', handleCardSubmit);
